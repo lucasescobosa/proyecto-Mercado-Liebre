8f84424c43e146b80928259c36359d72a4e2a887
lucasescobosa <lucasescobosa@gmail.com>
2022-08-03 21:26:12 -0300

    agregado paginas register y login

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ app.use(express.static('public'));
 app.listen(3000, () => console.log('Servidor corriendo'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
+
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'/views/register.html')));
+app.post('/crear',(req,res) => res.send('recibi un formulario'));
+
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'/views/login.html')));
