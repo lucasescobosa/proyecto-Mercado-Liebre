c1d0e47f0a8bc77f264e135a4759a42c46fc4019
lucasescobosa <lucasescobosa@gmail.com>
2022-08-11 16:38:49 -0300

    ultimos detalles de funcionalidad

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.listen(3000, () => console.log('Servidor corriendo'));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
 
 app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'/views/register.html')));
-app.post('/crear',(req,res) => res.send('recibi un formulario'));
+app.post('/register',(req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
 
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'/views/login.html')));
+app.post('/login',(req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
