5579befd927afc1563f185626bf6974ee4a3a055
lucasescobosa <lucasescobosa@gmail.com>
2022-08-15 19:26:56 -0300

    modificaciones para Heroku

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-
-app.listen(3000, () => console.log('Servidor corriendo'));
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'/views/home.html')));
 
